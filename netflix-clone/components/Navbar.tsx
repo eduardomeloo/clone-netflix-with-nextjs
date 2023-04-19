c9a19713a7ed32e4c9d4075692ad27123d7c9867
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-04-18 23:00:21 -0300

    ajuste remoção da marca

--- netflix-clone/components/Navbar.tsx
+++ netflix-clone/components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
         <nav className="w-full fixed z-40">
             <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition
                             duration-500 ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}>
-                <img className="h-4 lg:h-7" src="/images/logo.png" alt="Logo" />
+                <img className="h-16 lg:h-24" src="/images/logo.png" alt="Logo" />
                 <div className="flex-row ml-8 gap-7 hidden lg:flex">
                     <NavbarItem label="Home" />
                     <NavbarItem label="Series" />
--- netflix-clone/pages/auth.tsx
+++ netflix-clone/pages/auth.tsx
@@ -72,7 +72,7 @@ const Auth = () => {
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
                 <nav className="px-12 py-5">
-                    <img src="/images/logo.png" alt="Logo" className="h-12" />
+                    <img className="h-16 lg:h-24" src="/images/logo.png" alt="Logo" />
                 </nav>
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
@@ -115,7 +115,7 @@ const Auth = () => {
                             </div>
                         </div>
                         <p className="text-neutral-500 mt-12">
-                            {variant === 'login' ? 'Primeira vez usando Netflix?' : 'Já possui uma conta?' }
+                            {variant === 'login' ? 'Primeira vez aqui?' : 'Já possui uma conta?' }
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
                                 {variant === 'login' ? 'Crie uma conta' : 'Login'}
                             </span>
